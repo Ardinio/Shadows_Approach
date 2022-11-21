91f482bbf26a897666e9b6eda3ec4c6c93eed437
Ardi <70941433+Ardinio@users.noreply.github.com>
2022-11-21 10:22:06 +0100

    Updated jumping
    You can now jump higher and longer when jump button are hold down

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -5,28 +5,58 @@ public class PlayerJump : MonoBehaviour
     // Force, apply force, 1x
     // rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
-    [Header("None Serialized Field Vars")]
+    [Header("Jump Details")]
     public float jumpForce;
-    public bool grounded;
-    private Rigidbody2D rb;
+    public float jumpTime;
+    private float jumpTimeCounter;
+    private bool stoppedJumping;
 
-    [Header("Serialized Field Vars")]
+    [Header("Ground Details")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float radOCircle;
     [SerializeField] private LayerMask whatIsGround;
+    public bool grounded;
+
+    [Header("Components")]
+    private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpTimeCounter = jumpTime;
     }
 
     private void Update()
     {
+        // What is means to be grounded
         grounded = Physics2D.OverlapCircle(groundCheck.position, radOCircle, whatIsGround);
-        // Use space and/ or w to jump
+
+        if (grounded)
+        {
+            jumpTimeCounter = jumpTime;
+        }
+
+        // If we press the jump button
         if (Input.GetButtonDown("Jump") && grounded)
         {
+            // Jump!!!
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            stoppedJumping = false;
+        }
+
+        // If we hold the jump button
+        if (Input.GetButton("Jump") && !stoppedJumping && (jumpTimeCounter > 0))
+        {
+            // Jump!!!
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpTimeCounter -= Time.deltaTime;
+        }
+
+        // If we release the jump button
+        if (Input.GetButtonUp("Jump"))
+        {
+            jumpTimeCounter = 0;
+            stoppedJumping = true;
         }
     }
 
